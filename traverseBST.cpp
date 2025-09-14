a6a8e4776ed801763348d20871599c6d3463a6fe
Amanpreet Kapoor <kapooramanpreet@gmail.com>
2025-09-14 14:48:49 -0400

    Update and rename ImdbTree.cpp to traverseBST.cpp

--- traverseBST.cpp
+++ traverseBST.cpp
@@ -11,51 +11,57 @@ using namespace bridges;
 /*
   Step 1: Get the Bridges USER ID and API KEY for your program
           by creating an account and signing up (You can use any
-          email): https://bridges-cs.herokuapp.com/login
+          email): https://bridges-cs.herokuapp.com/signup
 
   Step 2: User ID (Username) and Key (API Sha1 Key) can be
           found under Profile on the top left
 */
 
-
 int main(int argc, char **argv)
 {
 
     // Step 3: Test if the following print statement is being run
-    cout << "Bridges: IMDB Data in a BST\n";
+    cout << "Bridges Test: I can see this is getting printed\n";
+
+    // Step 4: Add your User ID and API Key as secrets on Replit
+
+    /* Step 5: Print your User ID and API Key from secrets to the console
+       to test if secrets were properly loaded
+    */
 
     /*
-     Step 4: Create a Bridges object by uncommenting the next line
+     Step 6: Create a Bridges object by uncommenting the next line
      and inserting the values from steps 1 and 2
     */
-    //Bridges bridges(1, "USER_ID", "API_KEY");
+    Bridges bridges(1, "SECRET_HOLDING_USER_ID", "SECRET_HOLDING_API_KEY");
 
     /*
-       Step 5: Import IMDB data into a vector<ActorMovieIMDB> by
-               referring to the Bridges IMDB documentation:
-               https://bridgesuncc.github.io/tutorials/Data_IMDB.html
+       Step 7: Import your selected data into a vector<> by
+               referring to C++ examples in the Bridges datasets documentation:
+               https://bridgesuncc.github.io/datasets.html
     */
 
     /*
-       Step 6: Open the file "insertIntoBST.txt" and copy the provided
-               function for inserting an actor/movie pair into a BST.
+       Step 8: Open the file "insertIntoBST.txt" and copy the provided
+               function for inserting data into a BST.
                Paste the function into this file above your "main" function.
     */
 
     /*
-       Step 7: Use the insertion function from the previous step to insert
-               any 100 actor/movie pairs into a Bridges BST. Refer to the
-               Bridges IMDB documentation:
-               https://bridgesuncc.github.io/tutorials/Data_IMDB.html
+       Step 9: Use the insertion function from the previous step to build a BST with
+                100 data points. Refer to the Bridges documentation:
+               https://bridgesuncc.github.io/datasets.html
     */
 
+
     /*
-       Step 8: Visualize this tree by referring to the Bridges BST documentation:
+       Step 10: Visualize this tree by referring to the Bridges BST documentation:
                https://bridgesuncc.github.io/tutorials/BinarySearchTree.html
     */
 
+
     /*
-       Step 9: Color each level of the tree using a level-order traversal.
+       Step 11: Color each level of the tree using a level-order traversal.
                Every node in a given level should have the same color.
                Do not use the same color in two consecutive levels. A starter
                queue has been provided in case you wish to use an iterative
@@ -69,8 +75,8 @@ int main(int argc, char **argv)
     queue<BSTElement<string, string> *> q;
 
     /*
-      Step 10: Visualize the updated tree. Comment out or remove the old
-               visualization code from Step 8
+      Step 12: Visualize the updated tree. Comment out or remove the old
+               visualization code from Step 10
     */
 
     return 0;
